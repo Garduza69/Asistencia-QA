065a8f339a58e3f55ddb234ac4661fefabadfa7f
CarlosM <159581544+Ghostsoldier221@users.noreply.github.com>
2024-08-29 13:49:10 -0600

    obtener materias vigentes

--- obtener_materias.php
+++ obtener_materias.php
@@ -27,21 +27,35 @@ $stmt_alumno->execute();
 $row_alumno = $stmt_alumno->fetch(PDO::FETCH_ASSOC);
 $alumno_id = $row_alumno['alumno_id'];
 
-// Consultar el grupo_id asociado al alumno_id en la tabla matricula
-$sql_grupo = "SELECT grupo_id FROM matricula WHERE alumno_id = :alumno_id GROUP BY grupo_id";
-$stmt_grupo = $pdo->prepare($sql_grupo);
-$stmt_grupo->bindParam(':alumno_id', $alumno_id, PDO::PARAM_INT);
-$stmt_grupo->execute();
+// Consultar los grupo_id asociados al alumno_id en la tabla matricula
+$sql_grupos = "SELECT grupo_id FROM matricula WHERE alumno_id = :alumno_id";
+$stmt_grupos = $pdo->prepare($sql_grupos);
+$stmt_grupos->bindParam(':alumno_id', $alumno_id, PDO::PARAM_INT);
+$stmt_grupos->execute();
+
+// Recuperar todos los grupo_id asociados
+$grupos = $stmt_grupos->fetchAll(PDO::FETCH_ASSOC);
+
+// Si quieres guardar los IDs de los grupos en un array
+$grupo_ids = array_column($grupos, 'grupo_id');
+
+foreach ($grupo_ids as $grupo_id) {
+    // Consultar si el grupo_id está vigente en la tabla grupos
+    $sql_grupovig = "SELECT grupo_id FROM grupos WHERE grupo_id = :grupo_id AND vigenciaSem = 1 GROUP BY grupo_id";
+    $stmt_grupovig = $pdo->prepare($sql_grupovig);
+    $stmt_grupovig->bindParam(':grupo_id', $grupo_id, PDO::PARAM_INT);
+    $stmt_grupovig->execute();
+}
 
 $options = '';
-while ($row_grupo = $stmt_grupo->fetch(PDO::FETCH_ASSOC)) {
-    $grupo_id = $row_grupo['grupo_id'];
+while ($row_grupovig = $stmt_grupovig->fetch(PDO::FETCH_ASSOC)) {
+    $grupo_id1 = $row_grupovig['grupo_id'];
 
     $sql_materias = "SELECT a.materia_id AS materia_id, m.nombre AS nombre FROM matricula a 
                     JOIN materias m ON m.materia_id = a.materia_id
                     WHERE a.grupo_id = :grupo_id GROUP BY nombre";
     $stmt_materias = $pdo->prepare($sql_materias);
-    $stmt_materias->bindParam(':grupo_id', $grupo_id, PDO::PARAM_INT);
+    $stmt_materias->bindParam(':grupo_id', $grupo_id1, PDO::PARAM_INT);
     $stmt_materias->execute();
 
     while ($row = $stmt_materias->fetch(PDO::FETCH_ASSOC)) {
